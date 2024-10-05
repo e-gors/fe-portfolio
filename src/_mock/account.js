d64c639266f1406d3d9b5e424792060b606eb51e
Gors <goronefren@gmail.com>
2024-10-05 11:30:33 +0800

    changed _mock account img url to process env

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,9 +1,8 @@
 // ----------------------------------------------------------------------
-import Profile from "assets/images/avatars/m_avatar_5.jpg";
 
 export const account = {
-  displayName: "Efren Goron",
+  displayName: 'Efren Goron',
   role: "Super Admin",
-  email: "goronefren@gmail.com",
-  photoURL: Profile,
+  email: 'goronefren@gmail.com',
+  photoURL: `${process.env.PUBLIC_URL}/assets/images/avatars/m_avatar_5.jpg`,
 };
