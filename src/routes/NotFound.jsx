53d14d1c2a5ddc04d00cb0e065fac7d2fa097976
Gors <goronefren@gmail.com>
2024-10-14 21:59:57 +0800

    Added dark mode to dashboard, dev mode and not found pages

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,18 +1,20 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 
-import { RouterLink } from './components';
+import { RouterLink } from "./components";
 
-import Logo from '../components/logo';
-import { ContainedButton } from '../components/CustomButtons';
-import { useDispatch } from 'react-redux';
-import { setPage } from '../redux/actions/pageActions';
+import Logo from "../components/logo";
+import { ContainedButton } from "../components/CustomButtons";
+import { useDispatch, useSelector } from "react-redux";
+import { setPage } from "../redux/actions/pageActions";
 
 // ----------------------------------------------------------------------
 
 export default function NotFound() {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.theme);
+
   const renderHeader = (
     <Box
       component="header"
@@ -21,8 +23,11 @@ export default function NotFound() {
         left: 0,
         width: 1,
         lineHeight: 0,
-        position: 'fixed',
-        p: (theme) => ({ xs: theme.spacing(3, 3, 0), sm: theme.spacing(5, 5, 0) }),
+        position: "fixed",
+        p: (theme) => ({
+          xs: theme.spacing(3, 3, 0),
+          sm: theme.spacing(5, 5, 0),
+        }),
       }}
       onClick={() => dispatch(setPage("Home"))}
     >
@@ -37,37 +42,44 @@ export default function NotFound() {
       <Container>
         <Box
           sx={{
+            backgroundColor:
+              theme === "light" ? "var(--bg_lightest)" : "var(--bg_darkest)",
             py: 12,
             maxWidth: 480,
-            mx: 'auto',
-            display: 'flex',
-            minHeight: '100vh',
-            textAlign: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            justifyContent: 'center',
+            mx: "auto",
+            display: "flex",
+            minHeight: "100vh",
+            textAlign: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
           <Typography variant="h3" sx={{ mb: 3 }}>
             Sorry, page not found!
           </Typography>
 
-          <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be
-            sure to check your spelling.
+          <Typography sx={{ color: "text.secondary" }}>
+            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
+            mistyped the URL? Be sure to check your spelling.
           </Typography>
 
           <Box
             component="img"
             src="/assets/illustrations/illustration_404.svg"
             sx={{
-              mx: 'auto',
+              mx: "auto",
               height: 260,
               my: { xs: 5, sm: 10 },
             }}
           />
 
-          <ContainedButton href="/" size="large" variant="contained" component={RouterLink}>
+          <ContainedButton
+            href="/"
+            size="large"
+            variant="contained"
+            component={RouterLink}
+          >
             Go to Home
           </ContainedButton>
         </Box>
